a1fc57dbfd259f8f2841278fd1079873f7b53493
HauntTheHouse <romadurda2001@gmail.com>
2021-03-16 15:12:31 +0200

    remove build files

--- PlanetOfTanks/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/durda/Desktop/test task/task2021_linux/PlanetOfTanks
-# Build directory: /home/durda/Desktop/test task/task2021_linux/PlanetOfTanks/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
